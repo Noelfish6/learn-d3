699c8661ee0e403b29ce45631f78179af7187bb9
Zhengyan Yu <noel.fish6@gmail.com>
2020-03-08 22:31:02 +0800

    read the data

--- day 1/stripes.js
+++ day 1/stripes.js
@@ -1,3 +1,16 @@
+// read the data
 function readData(file, id) {
 	console.log("read the data")
+
+	d3.csv(file, processData) // promise object, has a then function
+		.then((data) => graph(data, id)) // callback, do something with arrary in d
+		.catch((error) => console.log("Error: ", error.message)); // callback
+}
+
+fucntion processData(datum){
+	return datum;	
+}
+
+fucntion graph(data, id){
+	console.log(id, data);
 }
