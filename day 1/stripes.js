b5fb6c47d42e536d6340dec8ec87082e8db44e68
Zhengyan Yu <noel.fish6@gmail.com>
2020-03-09 23:27:55 +0800

    data parsing

--- day 1/stripes.js
+++ day 1/stripes.js
@@ -1,16 +1,21 @@
 // read the data
 function readData(file, id) {
-	console.log("read the data")
+	console.log("read the data");
 
 	d3.csv(file, processData) // promise object, has a then function
 		.then((data) => graph(data, id)) // callback, do something with arrary in d
 		.catch((error) => console.log("Error: ", error.message)); // callback
 }
 
-fucntion processData(datum){
-	return datum;	
+function processData(datum){
+	let dataItem = {
+		year: parseFloat(datum.Year) || 0.00,
+		avg: parseFloat(datum["J-D"]) || 0.00
+	};
+
+	return dataItem;	
 }
 
-fucntion graph(data, id){
+function graph(data, id){
 	console.log(id, data);
 }
